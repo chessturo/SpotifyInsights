2abfc209d06df77dd27773be1a244b18f397c04c
levymitchell0 <levymitchell0@gmail.com>
2020-11-24 12:01:36 -0800

    Refactor server code to move boilerplate to helper methods.

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.net.*;
+import java.util.List;
+import java.util.function.*;
 
 import com.sun.net.httpserver.*;
 
@@ -30,21 +32,81 @@ public class Server {
 			Server.notFound = Server.class.getResourceAsStream("notfound.html").readAllBytes();
 		} catch (IOException ioe) {
 			System.err.println("Could not read a required resource: " + ioe);
+			System.exit(1);
 		}
 
+		HttpServer server = null;
 		try {
-			HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
-			server.createContext("/", (HttpExchange t) -> {
-				if (t.getRequestURI().getPath().equals("/") || t.getRequestURI().getPath().equals("/index.html")) {
-					Server.send(t, "text/html", index);
-				} else {
-					Server.send(t, "text/html", notFound, 404);
-				}
-			});
-			server.start();
+			server = HttpServer.create(new InetSocketAddress(PORT), 0);
 		} catch (IOException ioe) {
-			System.err.println("Failed to start server: " + ioe);
+			System.err.println("Failed to create server: " + ioe);
+			System.exit(1);
 		}
+
+		Server.addPath(server, "/", List.of("/", "/index.html"), (HttpExchange t) -> {
+			Server.send(t, "text/html", index);
+		});
+		server.start();
+	}
+
+	/**
+	 * Serves as a wrapper around
+	 * {@link com.sun.net.httpserver.HttpServer#createContext(String, HttpHandler)
+	 * HttpServer#createContext(String, HttpHandler)} to deal with boilerplate
+	 * around path matching. Using
+	 * {@code HttpServer#createContext(String, HttpHandler)}, the path {@code /}
+	 * will match all requests (as all requests will contain at least one {@code /}
+	 * in their path). When using this method, boilerplate for checking that the
+	 * matched path is what's intended (e.g., the handler for {@code /about.html}
+	 * will only match {@code /about.html}, not {@code /about.html-foo-bar}) is
+	 * added. If {@code HttpServer#createContext(String, HttpHandler)} matches a
+	 * request, but the path doesn't exactly match {@code path}, a response with a
+	 * 404 status and a body of {@link Server#notFound} is sent.
+	 * 
+	 * @param server   - The server that the handler is being added to. Cannot be
+	 *                 null.
+	 * @param path     - The path that must be matched.
+	 * @param callback - The callback to run if {@code path} exactly matches the
+	 *                 request path.
+	 */
+	private static void addPath(HttpServer server, String path, Consumer<HttpExchange> callback) {
+		addPath(server, path, List.of(path), callback);
+	}
+
+	/**
+	 * Serves as a wrapper around
+	 * {@link com.sun.net.httpserver.HttpServer#createContext(String, HttpHandler)
+	 * HttpServer#createContext(String, HttpHandler)} to deal with boilerplate
+	 * around path matching. Using
+	 * {@code HttpServer#createContext(String, HttpHandler)}, the path {@code /}
+	 * will match all requests (as all requests will contain at least one {@code /}
+	 * in their path). When using this method, boilerplate for checking that the
+	 * matched path is what's intended (e.g., the handler for {@code /} will only
+	 * match paths like {@code /}, {@code /index.html}, etc.) is added. If
+	 * {@code HttpServer#createContext(String, HttpHandler)} matches a request, but
+	 * the path is not contained within {@code paths}, a response with a 404 status
+	 * and a body of {@link Server#notFound} is sent.
+	 * 
+	 * @param server   - The server that the handler is being added to. Cannot be
+	 *                 null.
+	 * @param basePath - The base path, passed directly to
+	 *                 {@code HttpServer#createContext(String, HttpHandler)}. Cannot
+	 *                 be null.
+	 * @param paths    - The list of valid paths. This should include
+	 *                 {@code basePath} in the vast majority of cases. Cannot be
+	 *                 null.
+	 * @param callback - The callback to run if the request path is within
+	 *                 {@code paths}.
+	 */
+	private static void addPath(HttpServer server, String basePath, List<String> paths,
+			Consumer<HttpExchange> callback) {
+		server.createContext(basePath, (HttpExchange t) -> {
+			if (paths.contains(t.getRequestURI().getPath())) {
+				callback.accept(t);
+			} else {
+				Server.send(t, "text/html", notFound, HttpURLConnection.HTTP_NOT_FOUND);
+			}
+		});
 	}
 
 	/**
@@ -59,11 +121,15 @@ public class Server {
 	 * @param statusCode  - The HTTP status code used in the response.
 	 * @throws IOException
 	 */
-	private static void send(HttpExchange t, String contentType, byte[] content, int statusCode) throws IOException {
-		t.getResponseHeaders().set("Content-Type", contentType);
-		t.sendResponseHeaders(statusCode, content.length);
-		t.getResponseBody().write(content);
-		t.getResponseBody().close();
+	private static void send(HttpExchange t, String contentType, byte[] content, int statusCode) {
+		try {
+			t.getResponseHeaders().set("Content-Type", contentType);
+			t.sendResponseHeaders(statusCode, content.length);
+			t.getResponseBody().write(content);
+			t.getResponseBody().close();
+		} catch (IOException ioe) {
+			System.err.println("Error sending response: " + ioe);
+		}
 	}
 
 	/**
@@ -77,7 +143,7 @@ public class Server {
 	 * @param content     - The content of the response (i.e., the response body).
 	 * @throws IOException
 	 */
-	private static void send(HttpExchange t, String contentType, byte[] content) throws IOException {
-		Server.send(t, contentType, content, 200);
+	private static void send(HttpExchange t, String contentType, byte[] content) {
+		Server.send(t, contentType, content, HttpURLConnection.HTTP_OK);
 	}
 }
