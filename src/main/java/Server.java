fe1fdfd93604fa035929059b153386b307bf9f0d
Mitchell Levy <levymitchell0@gmail.com>
2020-11-27 15:35:05 -0800

    Working ability to connect to the Spotify API and access user data.

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -4,6 +4,8 @@ import java.nio.charset.*;
 import java.util.*;
 import java.util.function.*;
 
+import org.json.*;
+
 import com.sun.net.httpserver.*;
 
 /**
@@ -48,7 +50,7 @@ public class Server {
 	/**
 	 * The scopes that the application needs to have access to on the Spotify API.
 	 */
-	private static final String SPOTIFY_SCOPE = "user-read-email";
+	private static final String SPOTIFY_SCOPE = "user-read-email user-library-read";
 
 	/**
 	 * The name of the state cookie given to the user-agent to help prevent CSRF.
@@ -83,7 +85,7 @@ public class Server {
 			// Random hex value from 0x000000 -> 0xFFFFFF
 			int stateNumber = (int) (Math.random() * (0xFFFFFF + 1));
 			String state = String.format("%06x", stateNumber);
-			
+
 			Server.addCookie(t, STATE_COOKIE_NAME, state);
 			Server.redirect(t,
 					"https://accounts.spotify.com/authorize?" + "response_type=code" + "&client_id="
@@ -99,7 +101,7 @@ public class Server {
 			for (String cookieKvp : rawCookies) {
 				if (cookieKvp.startsWith(STATE_COOKIE_NAME + "=")) {
 					String[] stateCookie = cookieKvp.split("=", 2);
-					
+
 					// Handle if the header looks like
 					// Cookie: spotify_oauth_state=000000; Secure; HttpOnly
 					int endOfValue = stateCookie[1].indexOf(';');
@@ -107,7 +109,7 @@ public class Server {
 					cookieState = stateCookie[1].substring(0, endOfValue);
 				}
 			}
-			
+
 			String query = t.getRequestURI().getQuery();
 			Map<String, String> queryPairs = new HashMap<>();
 			String[] keyValuePairs = query.split("&");
@@ -120,7 +122,39 @@ public class Server {
 			queryState = queryPairs.get("state");
 
 			if (cookieState != null && queryState != null && cookieState.equals(queryState)) {
-				Server.send(t, "text/plain", "OAuth success.");
+				if (queryPairs.containsKey("error")) {
+					Server.send(t, "text/plain", "Error: " + queryPairs.get("error"));
+				} else {
+					// Get auth token from Spotify account service.
+					String authToken = null;
+					try {
+						URL spotifyAccountService = new URL("https://accounts.spotify.com/api/token");
+						String requestBody = "grant_type=authorization_code" + "&code=" + queryPairs.get("code")
+								+ "&redirect_uri=" + SPOTIFY_OAUTH_CALLBACK + "&client_id=" + spotifyClientId
+								+ "&client_secret=" + spotifyClientSecret;
+						String response = Server.makePostRequest(spotifyAccountService, requestBody);
+
+						authToken = new JSONObject(response).getString("access_token");
+					} catch (IOException ioe) {
+						System.err.println("Error connecting to the Spotify account service: " + ioe);
+					}
+
+					if (authToken == null) {
+						System.err.println("Auth token evaluated to null.");
+						Server.send(t, "text/plain", "Server error.", HttpURLConnection.HTTP_INTERNAL_ERROR);
+					} else {
+						String email = null;
+						try {
+							URL spotifyApi = new URL("https://api.spotify.com/v1/me");
+							String response = Server.makeGetRequest(spotifyApi, "application/json",
+									"Bearer " + authToken);
+							email = new JSONObject(response).getString("email");
+						} catch (IOException ioe) {
+							System.err.println("Error accessing the Spotify api: " + ioe);
+						}
+						Server.send(t, "text/plain", "OAuth success. Email: " + email);
+					}
+				}
 			} else {
 				Server.send(t, "text/plain", "State mismatch.", HttpURLConnection.HTTP_FORBIDDEN);
 			}
@@ -226,9 +260,9 @@ public class Server {
 	 * @param statusCode  - The HTTP status code used in the response.
 	 */
 	private static void send(HttpExchange t, String contentType, String content, int statusCode) {
-		Server.send(t,  contentType, content.getBytes(), statusCode);
+		Server.send(t, contentType, content.getBytes(), statusCode);
 	}
-	
+
 	/**
 	 * Using the given {@code HttpExchange}, sends a response with
 	 * {@code contentType} (as the {@code Content-Type} header) and {@code content}
@@ -242,7 +276,7 @@ public class Server {
 	private static void send(HttpExchange t, String contentType, String content) {
 		Server.send(t, contentType, content.getBytes());
 	}
-	
+
 	/**
 	 * Using the given {@code HttpExchange}, sends a response with
 	 * {@code contentType} (as the {@code Content-Type} header) and {@code content}
@@ -289,5 +323,95 @@ public class Server {
 	 */
 	private static void addCookie(HttpExchange t, String key, String value) {
 		t.getResponseHeaders().add("Set-Cookie", key + "=" + value);
+	}
+
+	/**
+	 * Makes a HTTP POST request to the specified {@code url}. The body of the
+	 * response is returned. {@code Content-Type} is assumed to be
+	 * {@code application/x-www-form-urlencoded"}, {@code Accept} is assumed to be
+	 * {@code application/json}.
+	 * 
+	 * @param url         - The URL to make the request to. Must be an HTTP URL
+	 *                    (i.e., have a protocol of http).
+	 * @param requestBody - The body of the post request.
+	 * @return The body of the server response.
+	 * @throws IOException If there are exceptions thrown by the IO methods involved
+	 *                     in making/writing to the connection.
+	 */
+	private static String makePostRequest(URL url, String requestBody) throws IOException {
+		return makePostRequest(url, requestBody, "application/x-www-form-urlencoded", "application/json", null);
+	}
+
+	/**
+	 * Makes a HTTP POST request to the specified {@code url}. The body of the
+	 * response is returned.
+	 * 
+	 * @param url           - The URL to make the request to. Must be an HTTP URL
+	 *                      (i.e., have a protocol of http).
+	 * @param requestBody   - The body of the post request.
+	 * @param contentType   - The value of the {@code Content-Type} header, sent
+	 *                      directly to the server.
+	 * @param accept        - The value of the {@code Accept} header, sent directly
+	 *                      to the server.
+	 * @param authorization - The value of the {@code Authorization} header, sent
+	 *                      directly to the server. If null, the header is omitted.
+	 * 
+	 * @return The body of the server response.
+	 * @throws IOException If there are exceptions thrown by the IO methods involved
+	 *                     in making/writing to the connection.
+	 */
+	private static String makePostRequest(URL url, String requestBody, String contentType, String accept,
+			String authorization) throws IOException {
+		HttpURLConnection accountServiceConnection = (HttpURLConnection) url.openConnection();
+		accountServiceConnection.setDoOutput(true);
+		accountServiceConnection.setRequestMethod("POST");
+		accountServiceConnection.setRequestProperty("Content-Type", contentType);
+		accountServiceConnection.setRequestProperty("Accept", accept);
+		if (authorization != null) {
+			accountServiceConnection.setRequestProperty("Authorization", authorization);
+		}
+		try (OutputStream req = accountServiceConnection.getOutputStream()) {
+			req.write(requestBody.getBytes());
+		}
+
+		String response;
+		try (InputStream res = accountServiceConnection.getInputStream()) {
+			response = (new String(res.readAllBytes()));
+		}
+		return response;
+	}
+
+	/**
+	 * Makes a HTTP GET request to the specified {@code url}. The body of the
+	 * response is returned.
+	 * 
+	 * @param url           - The URL to make the request to. Must be an HTTP URL
+	 *                      (i.e., have a protocol of http).
+	 * @param requestBody   - The body of the post request.
+	 * @param contentType   - The value of the {@code Content-Type} header, sent
+	 *                      directly to the server.
+	 * @param accept        - The value of the {@code Accept} header, sent directly
+	 *                      to the server.
+	 * @param authorization - The value of the {@code Authorization} header, sent
+	 *                      directly to the server. If null, the header is omitted.
+	 * 
+	 * @return The body of the server response.
+	 * @throws IOException If there are exceptions thrown by the IO methods involved
+	 *                     in making/writing to the connection.
+	 */
+	private static String makeGetRequest(URL url, String accept, String authorization) throws IOException {
+		HttpURLConnection accountServiceConnection = (HttpURLConnection) url.openConnection();
+		accountServiceConnection.setDoOutput(true);
+		accountServiceConnection.setRequestMethod("GET");
+		accountServiceConnection.setRequestProperty("Accept", accept);
+		if (authorization != null) {
+			accountServiceConnection.setRequestProperty("Authorization", authorization);
+		}
+
+		String response;
+		try (InputStream res = accountServiceConnection.getInputStream()) {
+			response = (new String(res.readAllBytes()));
+		}
+		return response;
 	}
 }
